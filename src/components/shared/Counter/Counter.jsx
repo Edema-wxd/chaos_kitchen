1738b16b48420394eb72d1eeff744be767eda716
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-08-17 09:04:36 +0100

    raw counter

--- src/components/shared/Counter/Counter.jsx
+++ src/components/shared/Counter/Counter.jsx
@@ -1,8 +1,35 @@
 import style from "./Counter.module.css"
+import { useState } from "react";
 
 function Counter() {
+  const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
   return (
-    <div className={style.main}>Counter</div>
+    <div className={style.main}>
+          <div className="counter">
+      <h1>React Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
+    </div>
+    </div>
   )
 }
 
