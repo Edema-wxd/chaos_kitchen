c7be1a364e7c803d21d902f9d7fe851c6a9de8f6
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-08-28 13:29:54 +0100

    fixed the cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,11 +1,29 @@
+import { async } from "q";
 import Pagechange from "../../animation/Pagechange";
 import Backbtn from "../../components/shared/Backbtn/Backbtn";
 import FoodCard from "../../components/shared/FoodCard/FoodCard";
 //import FoodCard from "../../components/shared/FoodCard/FoodCard";
 import style from "./Cart.module.css";
+import Gcontext from "../../context/Gcontext";
+import { useContext, useEffect, useState } from "react";
 
 function Cart() {
   const location = "cart";
+  const [cuserdata, setCuserdata] = useState([]);
+  const { rawdata } = useContext(Gcontext);
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  // fetch data from db.json
+  const fetchCart = async () => {
+    const response = await fetch(`http://localhost:5000/cart`);
+
+    const userdata = await response.json();
+
+    setCuserdata(userdata);
+  };
 
   return (
     <Pagechange>
@@ -15,12 +33,13 @@ function Cart() {
           <Backbtn location={location} />
         </div>
         <div className={style.Cfoods}>
-          <FoodCard item={1} location={location} />
-          <FoodCard item={1} location={location} />
-          <FoodCard item={1} location={location} />
-          <FoodCard item={1} location={location} />
-          <FoodCard item={1} location={location} />
-          <FoodCard item={1} location={location} />
+          {Object.keys(cuserdata).map((item) => (
+            <FoodCard
+              key={cuserdata[item].foodid}
+              item={rawdata[item]}
+              location={location}
+            />
+          ))}
         </div>
         <div className={style.Ccheckout}>
           <div className={style.CCpromo}>
